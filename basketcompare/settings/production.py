a8756eff7e6c74acce3882a6b4e7b7e407ed4d13
Jake <jakenwr@hotmail.co.uk>
2018-08-04 20:25:06 +0100

    h

--- basketcompare/settings/production.py
+++ basketcompare/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['hidden-sands-73111.herokuapp.com']
 
 MIDDLEWARE_CLASSES += ['whitenoise.middleware.WhiteNoiseMiddleware',]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
