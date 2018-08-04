323ce139c5775979846371f696ff49d6d42b488a
Jake <jakenwr@hotmail.co.uk>
2018-08-04 17:16:42 +0100

    email

--- basketcompare/settings/production.py
+++ basketcompare/settings/production.py
@@ -6,3 +6,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['hidden-sands-73111.herokuapp.com']
 
 django_heroku.settings(locals())
+
+EMAIL_HOST_USER = 'app104453826@heroku.com'
+EMAIL_HOST= 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = 'vr3fnnr76312'
