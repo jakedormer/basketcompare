6f3737849906bb8d71b144a205b3bc8759f45f52
Jake <jakenwr@hotmail.co.uk>
2018-08-04 19:05:38 +0100

    hi

--- basketcompare/wsgi.py
+++ basketcompare/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basketcompare.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basketcompare.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 
