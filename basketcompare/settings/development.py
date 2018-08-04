3a4a4a2a79825b377218e8aa356063961ebbf887
Jake <jakenwr@hotmail.co.uk>
2018-08-04 20:34:19 +0100

    h

--- basketcompare/settings/development.py
+++ basketcompare/settings/development.py
@@ -23,4 +23,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "staticfiles", "media")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
     )
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
