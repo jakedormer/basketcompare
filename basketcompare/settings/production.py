537099a0789c6dc3f2317aae9a1e7c50b1637331
Jake <jakenwr@hotmail.co.uk>
2018-08-05 19:15:21 +0100

    a

--- basketcompare/settings/production.py
+++ basketcompare/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['hidden-sands-73111.herokuapp.com']
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = (
