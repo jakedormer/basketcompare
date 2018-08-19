5ac5990c8d7fab4422b95d78690111d6f8f6dadf
Jake <jakenwr@hotmail.co.uk>
2018-08-19 20:57:24 +0100

    Removed whitenoise compression

--- basketcompare/settings/base.py
+++ basketcompare/settings/base.py
@@ -106,5 +106,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
